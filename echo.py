c42db6b8e96606134ff9d685328087d053cdb83b
Cheria Artis <cheriaa34@gmail.com>
2020-06-09 18:12:31 -0400

    "Initial Commit"

--- echo.py
+++ echo.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""An enhanced version of the 'echo' cmd line utility."""
+"""An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "Cheria Artis with help from Chris Warren"
 
 
 import sys
+import argparse
 
 
 def create_parser():
-    """Returns an instance of argparse.ArgumentParser"""
-    # your code here
-    return
+    """Creates and returns an argparse cmd line option parser."""
+    parser = argparse.ArgumentParser(description="Transform input text.")
+    parser.add_argument("-u", "--upper", help="convert text to uppercase", action="store_true")
+    parser.add_argument("-l", "--lower", help="convert text to lowercase", action="store_true")
+    parser.add_argument("-t", "--title", help="convert text to titlecase", action="store_true")
+    parser.add_argument("text", help="text to be manipulated")
+
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    # your code here
-    return
+    parser = create_parser()
+    
+    if not args:
+        parser.print_usage()
+        sys.exit()
+        
+    args = parser.parse_args(args)
+    text = args.text
 
+    if args.upper:
+        text = text.upper()
+    elif args.lower:
+        text = text.lower()
+    elif args.title:
+        text = text.title()
+    
+    return text
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    print(main(sys.argv[1:]))
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,123 +1,107 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Implements a test fixture for the echo.py module
 
-Students are expected to edit this module, to add more tests to run
-against the 'echo.py' program.
-"""
-
-__author__ = "???"
-
-import sys
-import importlib
-import inspect
-import argparse
 import unittest
+import echo
 import subprocess
-from io import StringIO
-
-# devs: change this to 'soln.echo' to run this suite against the solution
-PKG_NAME = 'echo'
 
 
-# This is a helper class for the main test class
-# Students can use this class object in their code
-class Capturing(list):
-    """Context Mgr helper for capturing stdout from a function call"""
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
-
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
-        sys.stdout = self._stdout
-
-
-# Students can use this function in their code
-def run_capture(pyfile, args=()):
-    """
-    Runs a python program in a separate process,
-    returns stdout and stderr outputs as 2-tuple
-    """
-    cmd = ["python", pyfile]
-    cmd.extend(args)
-    p = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-    stdout, stderr = p.communicate()
-    stdout = stdout.decode().splitlines()
-    stderr = stderr.decode().splitlines()
-    assert stdout or stderr, "The program is not printing any output"
-    return stdout, stderr
-
-
-# Student shall complete this TestEcho class so that all tests run and pass.
+# Your test case class goes here
 class TestEcho(unittest.TestCase):
-    """Main test fixture for 'echo' module"""
-    @classmethod
-    def setUpClass(cls):
-        """Performs module import and suite setup at test-runtime"""
-        # check for python3
-        cls.assertGreaterEqual(cls, sys.version_info[0], 3)
-        # This will import the module to be tested
-        cls.module = importlib.import_module(PKG_NAME)
-        # Make a dictionary of each function in the student's test module
-        cls.funcs = {
-            k: v for k, v in inspect.getmembers(
-                cls.module, inspect.isfunction
-                )
-            }
-        # check the module for required functions
-        assert "main" in cls.funcs, "Missing required function main()"
-        assert "create_parser" in cls.funcs, "Missing required function create_parser()"
-
     def setUp(self):
-        """Called by parent class ONCE before all tests are run"""
-        # your code here - use this space to create any instance variables
-        # that will be visible to your other test methods
-        pass
+        self.parser = echo.create_parser()
+        
+        
+    def test_help(self):
+        """Testing help option"""
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout = subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
 
-    def test_parser(self):
-        """Check if create_parser() returns a parser object"""
-        result = self.module.create_parser()
-        self.assertIsInstance(
-            result, argparse.ArgumentParser,
-            "create_parser() function is not returning a parser object")
-
-    #
-    # Students: add more parser tests here
-    #
-
-    def test_echo(self):
-        """Check if main() function prints anything at all"""
-        stdout, stderr = run_capture(self.module.__file__)
-        pass
-
-    def test_simple_echo(self):
-        """Check if main actually echoes an input string"""
-        args = ['Was soll die ganze Aufregung?']
-        stdout, stderr = run_capture(self.module.__file__, args)
-        self.assertEqual(
-            stdout[0], args[0],
-            "The program is not performing simple echo"
-            )
-
+        self.assertEquals(stdout.decode("utf8"), usage)
+        
+        
+    def test_upper_short(self):
+        """Testing upper short option"""
+        args = ["-u", "hello"]
+        namespace = self.parser.parse_args(args)
+        self.assertTrue(namespace.upper)
+        result = echo.main(args)
+        self.assertEquals(result, "HELLO")
+        
+        
+    def test_upper_long(self):
+        """Testing upper long option"""
+        args = ["--upper", "hello"]
+        namespace = self.parser.parse_args(args)
+        self.assertTrue(namespace.upper)
+        result = echo.main(args)
+        self.assertEquals(result, "HELLO")
+        
+        
     def test_lower_short(self):
-        """Check if short option '-l' performs lowercasing"""
-        args = ["-l", "HELLO WORLD"]
-        with Capturing() as output:
-            self.module.main(args)
-        assert output, "The program did not print anything."
-        self.assertEqual(output[0], "hello world")
-
-    #
-    # Students: add more cmd line options tests here.
-    #
-
-
+        """Testing lower short option"""
+        args = ["-l", "heLLo"]
+        namespace = self.parser.parse_args(args)
+        self.assertTrue(namespace.lower)
+        result = echo.main(args)
+        self.assertEquals(result, "hello")
+        
+        
+    def test_lower_long(self):
+        """Testing lower long option"""
+        args = ["--lower", "heLLo"]
+        namespace = self.parser.parse_args(args)
+        self.assertTrue(namespace.lower)
+        result = echo.main(args)
+        self.assertEquals(result, "hello")
+        
+        
+    def test_title_short(self):
+        """Testing title short option"""
+        args = ["-t", "hi folks!"]
+        namespace = self.parser.parse_args(args)
+        self.assertTrue(namespace.title)
+        result = echo.main(args)
+        self.assertEquals(result, "Hi Folks!")
+        
+        
+    def test_title_long(self):
+        """Testing title short option"""
+        args = ["--title", "hi folks!"]
+        namespace = self.parser.parse_args(args)
+        self.assertTrue(namespace.title)
+        result = echo.main(args)
+        self.assertEquals(result, "Hi Folks!")
+        
+        
+    def test_all(self):
+        """Testing all args"""
+        args = ["-tul", "heLLo!"]
+        namespace = self.parser.parse_args(args)
+        self.assertTrue(namespace.upper)
+        result = echo.main(args)
+        self.assertEquals(result, "HELLO!")
+        
+        
+    def test_two_args(self):
+        """Testing TWO args"""
+        args = ["-tl", "heLLo!"]
+        namespace = self.parser.parse_args(args)
+        self.assertTrue(namespace.lower)
+        result = echo.main(args)
+        self.assertEquals(result, "hello!")
+        
+        
+    def test_no_args(self):
+        """Testing NO args"""
+        args = ["Hello!"]
+        result = echo.main(args)
+        self.assertEquals(result, 'Hello!')
+        
+        
 if __name__ == '__main__':
     unittest.main()
+    
